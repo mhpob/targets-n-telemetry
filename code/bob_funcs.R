bdb39dff03f0cb9e8238064b1b40b6538b586821
Mike O'Brien <mikeob9@gmail.com>
2024-01-30 12:26:11 -0500

    add update

--- code/bob_funcs.R
+++ code/bob_funcs.R
@@ -4,5 +4,5 @@ bob_summer <- function(detections){
   bob <- detections[grepl('bob', detections$id),]
   
   bob[bob$datetime >= as.POSIXct('2023-06-01 00:00:00') &
-        bob$datetime <= as.POSIXct('2023-09-01 00:00:00'),]
+        bob$datetime <= as.POSIXct('2023-10-31 00:00:00'),]
 }
